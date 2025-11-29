593ea80290377b53a16884c14d7991e4417dd10a
ChaHyunWoo <2471005@hansung.ac.kr>
2025-11-29 11:07:10 +0900

    Solve Backjoon 2439

---
+++ src/q2439/Main.java
@@ -0,0 +1,21 @@
+package q2439;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			// n-i가 핵심임
+			for(int a=0;a<n-i-1;a++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
