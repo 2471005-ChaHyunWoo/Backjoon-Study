cf8e431f683333723652d24874cc37f01568163e
ChaHyunWoo <2471005@hansung.ac.kr>
2025-11-30 12:18:31 +0900

    Solve Backjoon 11720

---
+++ src/q11720/Main.java
@@ -0,0 +1,20 @@
+package q11720;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		String b=sc.next();
+		int sum=0;
+		// 숫자로 받아서 10으로 나누는 방식 런타임 에러
+		for(int i=0;i<a;i++) {
+			sum+=b.charAt(i)-'0';
+			// charAt으로 문자 하나씩 받아와야함
+		}
+		System.out.print(sum);
+	}
+
+}
